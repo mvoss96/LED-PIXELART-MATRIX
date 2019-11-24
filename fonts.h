c4977debdc5afb6b4569c18c13d25e9023a376c9
Marcus Voß <meav33@gmail.com>
2019-11-24 18:15:49 +0100

    added const to PROGMEM

--- fonts.h
+++ fonts.h
@@ -3,7 +3,7 @@
 
 //currently PROGMEM in class members doesnt work on ESP8266
 #ifdef ESP8266
-#define PROG_MEM 
+#define PROG_MEM
 #else
 #define PROG_MEM PROGMEM
 #endif
@@ -13,17 +13,17 @@ class font5x3
   private:
     byte fontwidth = 3;
     byte fontheight = 5;
-    bool n1[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
-    bool n2[15] PROG_MEM = {1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1};
-    bool n3[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1};
-    bool n4[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1};
-    bool n5[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1};
-    bool n6[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1};
-    bool n7[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
-    bool n8[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1};
-    bool n9[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1};
-    bool n0[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
-    bool nc[15] PROG_MEM = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0};
+    const bool n1[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+    const bool n2[15] PROG_MEM = {1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1};
+    const bool n3[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1};
+    const bool n4[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1};
+    const bool n5[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1};
+    const bool n6[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1};
+    const bool n7[15] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
+    const bool n8[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1};
+    const bool n9[15] PROG_MEM = {1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1};
+    const bool n0[15] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
+    const bool nc[15] PROG_MEM = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0};
 
   public:
     font5x3() {}
@@ -32,36 +32,37 @@ class font5x3
     //The Method then goes through the input string and chooses the right array containing boolean pixeldata (1 = foreground color, 0 = background color)
     void drawNumber(Matrix &matrix, byte s_x, byte s_y, String s, CRGB c1 = CRGB::White, CRGB c2 = CRGB::Black) //Draw a  Number at x,y
     {
-      bool * number;
+      bool * numberTemp; //temporary non const pointer to store the right address
       for (byte i = 0; i < s.length(); i++) //go through the input string s f.e. "10"
       {
         byte x = s_x + (fontwidth + 1) * i; //x,y drawing position  for the current number
         byte y = s_y;
         switch (s[i]) //chose the right array for the current nuber
         {
-          case '1': number = n1;
+          case '1': numberTemp = (bool*)n1;
             break;
-          case '2': number = n2;
+          case '2': numberTemp = (bool*)n2;
             break;
-          case '3': number = n3;
+          case '3': numberTemp = (bool*)n3;
             break;
-          case '4': number = n4;
+          case '4': numberTemp = (bool*)n4;
             break;
-          case '5': number = n5;
+          case '5': numberTemp = (bool*)n5;
             break;
-          case '6': number = n6;
+          case '6': numberTemp = (bool*)n6;
             break;
-          case '7': number = n7;
+          case '7': numberTemp = (bool*)n7;
             break;
-          case '8': number = n8;
+          case '8': numberTemp = (bool*)n8;
             break;
-          case '9': number = n9;
+          case '9': numberTemp = (bool*)n9;
             break;
-          case 'c': number = nc;
+          case 'c': numberTemp = (bool*)nc;
             break;
-          default: number = n0;
+          default: numberTemp = (bool*)n0;
             break;
         }
+        const bool* number = numberTemp; //make it const again
         for (byte column = 1, counter = 0; column <=  fontheight; column++) //Set the right pixels in the led matrix based on the array chosen above.
         {
           for (byte row = 1; row <= fontwidth; row++, counter++)
@@ -81,53 +82,54 @@ class font7x3
   private:
     byte fontwidth = 3;
     byte fontheight = 7;
-    bool n1[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
-    bool n2[21] PROG_MEM = {1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
-    bool n3[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
-    bool n4[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1};
-    bool n5[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1};
-    bool n6[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1};
-    bool n7[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
-    bool n8[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
-    bool n9[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
-    bool n0[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
-    bool nc[21] PROG_MEM = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0};
+    const bool n1[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1};
+    const bool n2[21] PROG_MEM = {1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
+    const bool n3[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
+    const bool n4[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1};
+    const bool n5[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1};
+    const bool n6[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1};
+    const bool n7[21] PROG_MEM = {0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1};
+    const bool n8[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
+    const bool n9[21] PROG_MEM = {1, 1, 1, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
+    const bool n0[21] PROG_MEM = {1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1};
+    const bool nc[21] PROG_MEM = {1, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 1, 0};
 
   public:
     font7x3() {}
     ~font7x3() {}
     void drawNumber(Matrix &matrix, byte s_x, byte s_y, String s, CRGB c1 = CRGB::White, CRGB c2 = CRGB::Black) //Draw a  Number at x,y
     {
-      bool * number;
+      bool * numberTemp; //temporary non const pointer to store the right address
       for (byte i = 0; i < s.length(); i++) //go through the input string s f.e. "10"
       {
         byte x = s_x + (fontwidth + 1) * i; //x,y drawing position  for the current number
         byte y = s_y;
         switch (s[i]) //chose the right array for the current nuber
         {
-          case '1': number = n1;
+          case '1': numberTemp = (bool*)n1;
             break;
-          case '2': number = n2;
+          case '2': numberTemp = (bool*)n2;
             break;
-          case '3': number = n3;
+          case '3': numberTemp = (bool*)n3;
             break;
-          case '4': number = n4;
+          case '4': numberTemp = (bool*)n4;
             break;
-          case '5': number = n5;
+          case '5': numberTemp = (bool*)n5;
             break;
-          case '6': number = n6;
+          case '6': numberTemp = (bool*)n6;
             break;
-          case '7': number = n7;
+          case '7': numberTemp = (bool*)n7;
             break;
-          case '8': number = n8;
+          case '8': numberTemp = (bool*)n8;
             break;
-          case '9': number = n9;
+          case '9': numberTemp = (bool*)n9;
             break;
-          case 'c': number = nc;
+          case 'c': numberTemp = (bool*)nc;
             break;
-          default: number = n0;
+          default: numberTemp = (bool*)n0;
             break;
         }
+        const bool* number = numberTemp; //make it const again
         for (byte column = 1, counter = 0; column <=  fontheight; column++) //Set the right pixels in the led matrix based on the array chosen above.
         {
           for (byte row = 1; row <= fontwidth; row++, counter++)
